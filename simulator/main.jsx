78c276beca4e295190c67d844ed0caea2bdfc000
MOHAMAD SAMY ARIDHAN HON <147996096+SamyAridhan@users.noreply.github.com>
2025-12-12 12:33:13 +0800

    Delete simulator/main.jsx

--- simulator/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import SmartIDDemo from './SmartIDDemo.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <SmartIDDemo />
-  </React.StrictMode>,
-)
